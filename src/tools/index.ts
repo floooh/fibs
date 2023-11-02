a5269f45a2c1efe9bd35010a7419002855f4b26e
Andre Weissflog <floooh@gmail.com>
2023-11-02 12:44:30 +0100

    remove tar tool (moved to fibs-platforms/wasi.ts)

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -3,13 +3,11 @@ import { cmakeTool } from './cmake.ts';
 import { gitTool } from './git.ts';
 import { makeTool } from './make.ts';
 import { vscodeTool } from './vscode.ts';
-import { tarTool } from './tar.ts';
 
 export const tools = [
     gitTool,
     cmakeTool,
     ninjaTool,
     makeTool,
-    tarTool,
     vscodeTool,
 ];
